21711b2201600c05642d8d5399b822f845887af4
Hipodbehu <hipodbehu@gmail.com>
2023-07-23 21:47:31 +0300

    Multiple components, added Navbar and Home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   const title = "Welcome to the new block"
@@ -8,7 +10,9 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
+        <Home />
         <h1>{ title }</h1>
         <p>Liked in { likes } times</p>
         {/*<p> { person } </p>*/}
