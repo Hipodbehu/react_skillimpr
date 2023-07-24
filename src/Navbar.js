8e6946ca681b5eef47ef92b2114328638f2d3112
Hipodbehu <hipodbehu@gmail.com>
2023-07-24 16:17:47 +0300

    Added styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,11 @@ const Navbar = () => {
       <h1>The Dojo Blog</h1>
       <div className="links">
         <a href="/">Home </a>
-        <a href="/create">Create </a>
+        <a href="/create" style={{
+          color: 'purple',
+          backgroundColor: 'black',
+          borderRadius: '7px'
+        }}>Create </a>
       </div>
     </nav>
   );
