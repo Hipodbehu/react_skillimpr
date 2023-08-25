e22218241afd5a1f5badc87f664a066238004b98
Hipodbehu <hipodbehu@gmail.com>
2023-08-25 16:13:12 +0300

    Passed function as prop

--- src/Home.js
+++ src/Home.js
@@ -9,27 +9,14 @@ const Home = () => {
     { title: 'Web dev tooltips', body: 'lrem ipms', author: 'Derek', id: 3}
   ])
 
-  const [name, setName] = useState('test_name');
-  const [age, setAge] = useState(23);
-
-  const handleClick = (e) => {
-    console.log('Hey ninja!', e);
-  }
-
-  const handleClickAgain = (name, e) => {
-    console.log('Heey ' + name + e.target);
-    setName('test_name2');
-    setAge(age + 1);
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
   }
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title={'Sometitle'}/>
-      <BlogList blogs={blogs.filter((blog) => blog.author === "Jarik")} title={'Check title'}/>
-      <h1>Homepage</h1>
-      <p>{ name } is { age } yrs old</p>
-      <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain('Jarik', e)}>Click me again</button>
+      <BlogList blogs={blogs} title={'Sometitle'} handleDelete={handleDelete}/>
     </div>
   )
 }
