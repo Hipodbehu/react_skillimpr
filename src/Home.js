4e4cc8eb657df6d6de963edced17cd9df16aa395
Hipodbehu <hipodbehu@gmail.com>
2023-09-01 16:14:29 +0300

    Fetched data from Json server

--- src/Home.js
+++ src/Home.js
@@ -3,29 +3,21 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body:'lrem ipms', author: 'Jarik', id: 1},
-    { title: 'Party heayahee', body: 'lrem ipms', author: 'Yaarik', id: 2},
-    { title: 'Web dev tooltips', body: 'lrem ipms', author: 'Derek', id: 3}
-  ])
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
-
-  const [name, setName] = useState('mario');
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log('Use effect ran')
-    console.log(name)
-  }, [name]);
+    fetch('http://localhost:8000/blogs')
+      .then(resp => {
+        return resp.json();
+      })
+      .then(data => {
+        setBlogs(data);
+      })
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title={'Sometitle'} handleDelete={handleDelete}/>
-      <button onClick={() => setName('luigi')}>change name</button>
-      <p>{ name }</p>
+      {blogs && <BlogList blogs={blogs} title={'Sometitle'} /> }
     </div>
   )
 }
