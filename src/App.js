7fe1fdab9333bdb27435cd4828f9344c7088ec83
Hipodbehu <hipodbehu@gmail.com>
2023-09-01 16:26:01 +0300

    Code reformatting, added Loading message and timeout for testing reasons

--- src/App.js
+++ src/App.js
@@ -2,23 +2,11 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
-  const title = "Welcome to the new block"
-  const likes = 50;
-  const link = "http://www.google.com";
-
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         <Home />
-        <h1>{ title }</h1>
-        <p>Liked in { likes } times</p>
-        <p>{ 10 }</p>
-        <p>{ "Hello ninjas!" }</p>
-        <p>{ [1,2,4] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-        <a href={ link }>Google site</a>
       </div>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,28 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import {wait} from "@testing-library/user-event/dist/utils";
 
 const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(resp => {
-        return resp.json();
-      })
-      .then(data => {
-        setBlogs(data);
-      })
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(resp => {
+          return resp.json();
+        })
+        .then(data => {
+          setBlogs(data);
+          setIsPending(false)
+        })
+    }, 1000)
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div>LOADING . . . </div> }
       {blogs && <BlogList blogs={blogs} title={'Sometitle'} /> }
     </div>
   )
